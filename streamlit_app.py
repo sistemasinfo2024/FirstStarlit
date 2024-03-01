9db1b97daaf2b08a41761443ff894b96b929a74e
sistemasinfo2024 <161856932+sistemasinfo2024@users.noreply.github.com>
2024-03-01 13:10:45 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,29 +1,13 @@
-import altair as alt
-import numpy as np
-import pandas as pd
 import streamlit as st
-import seaborn as sns
-"""
-
-# Primer App Starlit
-
-Aplicaciones de negocio implementadas en Streamlit
 
 """
 
-data = pd.read_excel('https://github.com/roscha10/ProyectoM6_machine_learning/raw/main/Propuesta%201/BBDD_Hospitalizaci%C3%B3n.xlsx')
+# Starlit Application
 
-print(data)
+"""
 
-st.text('Fixed width text')
-st.markdown('_Markdown_') # see #*
-st.caption('Balloons. Hundreds of them...')
-st.latex(r''' e^{i\pi} + 1 = 0 ''')
-st.write('Most objects') # df, err, func, keras!
-st.write(['st', 'is <', 3]) # see *
-st.title('My title')
-st.header('My header')
-st.subheader('My sub')
-st.code('for i in range(8): foo()')
+st.text("Información Personal")
+t_nombre = st.text_input("Nombre: ", autocomplete="Nombre")
+t_apellido = st.text_input("Apellido: ", autocomplete="Apellido")
 
-# * optional kwarg unsafe_allow_html = True
+st.text(f'Hola {t_nombre} {t_apellido}')
