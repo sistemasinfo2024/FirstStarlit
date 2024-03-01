74c097e92941ee795a2d89960ebddde0c89400c3
sistemasinfo2024 <161856932+sistemasinfo2024@users.noreply.github.com>
2024-03-01 12:35:43 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
+import altair as alt
+import numpy as np
+import pandas as pd
 import streamlit as st
-
+import seaborn as sns
 """
 
 # Primer App Starlit
@@ -7,3 +10,7 @@ import streamlit as st
 Aplicaciones de negocio implementadas en Streamlit
 
 """
+
+data = pd.read_excel('https://github.com/roscha10/ProyectoM6_machine_learning/raw/main/Propuesta%201/BBDD_Hospitalizaci%C3%B3n.xlsx')
+
+print(data)
