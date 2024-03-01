fd31e212982176c7e1b85e624cb538a51dd259f8
sistemasinfo2024 <161856932+sistemasinfo2024@users.noreply.github.com>
2024-03-01 13:19:38 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,19 @@
 import streamlit as st
+import pandas as pd
 
-"""
 
-# Starlit Application
-
-"""
-
+st.title("First Stalit Application")
 st.text("Información Personal")
 t_nombre = st.text_input("Nombre: ", autocomplete="Nombre")
 t_apellido = st.text_input("Apellido: ", autocomplete="Apellido")
 
-st.text(f'Hola {t_nombre} {t_apellido}')
+"""
+
+### Los datos del dataset de salud
+
+"""
+
+# data = pd.DataFrame([[t_nombre, t_apellido]], columns=['Nombre', 'Apellido'])
+data = pd.read_excel('https://github.com/roscha10/ProyectoM6_machine_learning/raw/main/Propuesta%201/BBDD_Hospitalizaci%C3%B3n.xlsx')
+
+st.dataframe(data)
