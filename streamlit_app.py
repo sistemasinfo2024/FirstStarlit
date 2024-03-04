d833671117facec5eaefcb76b54ce7c302b7e0d3
sistemasinfo2024 <161856932+sistemasinfo2024@users.noreply.github.com>
2024-03-04 11:12:18 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,11 @@ t_apellido = st.text_input("Apellido: ", autocomplete="Apellido")
 # data = pd.DataFrame([[t_nombre, t_apellido]], columns=['Nombre', 'Apellido'])
 data = pd.read_excel('https://github.com/roscha10/ProyectoM6_machine_learning/raw/main/Propuesta%201/BBDD_Hospitalizaci%C3%B3n.xlsx')
 
-sns.histplot(data['EDAD'])
+
 
 fig, ax = plt.subplots()
+
+sns.histplot(data['EDAD'])
+plt.show()
+
 st.pyplot(fig)
